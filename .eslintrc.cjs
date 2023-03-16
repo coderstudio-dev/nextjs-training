d120c4f790e1533a21a2e634585288b4d91dfca1
Jan Basoc <jbasoc@stratpoint.com>
2023-03-16 00:35:49 +0000

    build: implement initial storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,29 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      files: ['*.ts', '*.tsx'],
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-    },
-  ],
+  overrides: [{
+    extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
+    files: ['*.ts', '*.tsx'],
+    parserOptions: {
+      project: 'tsconfig.json'
+    }
+  }],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   plugins: ['@typescript-eslint'],
-  extends: [
-    'next',
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
   globals: {
-    React: 'readonly',
+    React: 'readonly'
   },
   rules: {
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'inline-type-imports',
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-  },
+    '@typescript-eslint/consistent-type-imports': ['warn', {
+      prefer: 'type-imports',
+      fixStyle: 'inline-type-imports'
+    }],
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      argsIgnorePattern: '^_'
+    }]
+  }
 };
-
 module.exports = config;
