1293f59a03ce0a95c699bf888222064a57ba2f46
Bryan Matias <bmmatias@stratpoint.com>
2023-03-29 12:41:47 +0000

    fix: error fix

--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Button from "../../utils/Button/Button";
+import Button from "../../Button/Button";
 
 export interface IHeader {
   isLoggedIn: boolean;
