0f9f7c55b448c48c9d69e67bf9f3d84579eab8ae
Bryan Matias <bmmatias@stratpoint.com>
2023-03-30 03:23:49 +0000

    fix: fixed header position

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,70 +12,72 @@ const Header: React.FC<IHeader> = ({ ...headerProps }) => {
   return (
     <header
       {...headerProps}
-      className="-full sticky top-0 z-10 flex flex-row justify-between border-b-2 border-slate-100 bg-white"
+      className=" bg-white"
     >
-      <div className="m-2 flex justify-center space-x-2">
-        <Button
-          content={
-            <Image
-              src="assets/menu-icon.svg"
-              alt="Logo"
-              width={25}
-              height={25}
-            />
-          }
-          buttonType="btn-transparent"
-          className="hidden max-md:block p-1"
-        />
-        <Link href="/">
-          <Image
-            src="/dev_logo.png"
-            alt="Logo"
-            width={50}
-            height={50}
-            priority
+      <div className="container mx-auto w-full sticky top-0 z-10 flex flex-row justify-between border-b-2 border-slate-100">
+        <div className="m-2 flex justify-center space-x-2">
+          <Button
+            content={
+              <Image
+                src="assets/menu-icon.svg"
+                alt="Logo"
+                width={25}
+                height={25}
+              />
+            }
+            buttonType="btn-transparent"
+            className="hidden max-md:block p-1"
           />
-        </Link>
-        <form className="relative max-md:hidden">
-          <input
-            type="text"
-            className="z-0 h-10 w-96 rounded-lg border border-slate-300 pl-2 outline-indigo-600 focus:border-none focus:outline-2"
-            placeholder="Search..."
-          />
-          <div className="absolute top-0 right-0">
-            <Button
-              content={
-                <Image
-                  src="/assets/search-icon.svg"
-                  alt="Logo"
-                  width={25}
-                  height={25}
-                />
-              }
-              buttonType="btn-transparent"
-            />
-          </div>
-        </form>
-      </div>
-      <div className="align-center m-2 flex space-x-2">
-        <Button
-          content={
+          <Link href="/">
             <Image
-              src="/assets/search-icon.svg"
+              src="/dev_logo.png"
               alt="Logo"
-              width={25}
-              height={25}
+              width={50}
+              height={50}
+              priority
             />
-          }
-          buttonType="btn-transparent"
-          className="hidden max-md:block p-1"
-        />
-        <Button className="max-md:hidden" content="Log in" buttonType="btn-transparent" />
-        <Button
-          content="Create Account"
-          buttonType="btn-outline"
-          className="max-md:text-sm"
-        />
+          </Link>
+          <form className="relative max-md:hidden">
+            <input
+              type="text"
+              className="z-0 h-10 w-96 rounded-lg border border-slate-300 pl-2 outline-indigo-600 focus:border-none focus:outline-2"
+              placeholder="Search..."
+            />
+            <div className="absolute top-0 right-0">
+              <Button
+                content={
+                  <Image
+                    src="/assets/search-icon.svg"
+                    alt="Logo"
+                    width={25}
+                    height={25}
+                  />
+                }
+                buttonType="btn-transparent"
+              />
+            </div>
+          </form>
+        </div>
+        <div className="align-center m-2 flex space-x-2">
+          <Button
+            content={
+              <Image
+                src="/assets/search-icon.svg"
+                alt="Logo"
+                width={25}
+                height={25}
+              />
+            }
+            buttonType="btn-transparent"
+            className="hidden max-md:block p-1"
+          />
+          <Button className="max-md:hidden" content="Log in" buttonType="btn-transparent" />
+          <Button
+            content="Create Account"
+            buttonType="btn-outline"
+            className="max-md:text-sm"
+          />
+        </div>
       </div>
     </header>
   );
